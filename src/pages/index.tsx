9ca43a805e1ff66c28e3290123606a48571e3377
Reginald Johnson <reginald.johnson@gmail.com>
2020-06-12 17:25:39 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Link, graphql, useStaticQuery } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 import Footer from "../components/Footer";
-import ScrollToTopButton from "../components/ScrollToTopButton";
-import AnimatedHeader from "../components/AnimatedHeader";
 import BlogPostList from "../components/BlogPostList";
 import Header from "../components/Header";
 import styled from "styled-components";
