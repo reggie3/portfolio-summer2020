fdeacadc15ed5994a25bd9c6441890161d06ab0d
Reginald Johnson <reginald.johnson@gmail.com>
2020-06-12 16:50:53 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, graphql, useStaticQuery } from "gatsby";
 
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 import Footer from "../components/Footer";
